d7fef3e036434551c2ecfb9713322a7c4143a5ff
Luka Maric <lukamaric718@gmail.com>
2026-06-03 10:15:00 +0200

    Defined core Actor, Message, Props, and lifecycle interfaces

---
+++ framework/actor.go
@@ -0,0 +1,27 @@
+package framework
+
+type Message = any
+
+type Actor interface {
+	Receive(ctx *ActorContext, msg Message)
+}
+
+type Behavior func(ctx *ActorContext, msg Message)
+
+type ActorFunc Behavior
+
+func (f ActorFunc) Receive(ctx *ActorContext, msg Message) { f(ctx, msg) }
+
+type PreStarter interface {
+	PreStart(ctx *ActorContext)
+}
+
+type PostStopper interface {
+	PostStop(ctx *ActorContext)
+}
+
+type Props struct {
+	Factory func() Actor
+
+	MailboxCapacity int
+}
